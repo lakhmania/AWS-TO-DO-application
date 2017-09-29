899c6052c8b86ecace55222bb2eaa9cb81c3d28e
Neha Lalwani <lalwani.n@husky.neu.edu>
2017-09-28 22:48:29 -0400

    adding jmeter files and team member info

--- src/main/java/com/csye6225/demo/repo/UserSessionRepository.java
+++ src/main/java/com/csye6225/demo/repo/UserSessionRepository.java
@@ -1,3 +1,10 @@
+/**
+ * <Neha Lalwani>, <001268916>, <lalwani.n@husky.neu.edu>
+ * <Nirali Merchant>, <001268909>, <merchant.n@husky.neu.edu>
+ * <Chintan Koticha>, <001267049>, <koticha.c@husky.neu.edu>
+ * <Apoorva Lakhmani>, <001256312>, <lakhmani.a@husky.neu.edu>
+ */
+
 package com.csye6225.demo.repo;
 
 import com.csye6225.demo.pojo.User;
@@ -10,7 +17,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserSessionRepository extends CrudRepository<UserSession,Long>{
 
-    //@Query("select u.userName from User u, UserSession us where u.userId = us.userId " +"and us.sessionId =?1")
+
     @Query("select u.userName from User u where u = (select us.user from UserSession us where us.sessionId = ?1)")
     String findUserNameBySessionId(@Param("sessionId") String sessionId);
 }
