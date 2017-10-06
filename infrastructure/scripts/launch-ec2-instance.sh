ec7d29c4ada842caeaaaf25fbf84b20642dde538
Neha Lalwani <lalwani.n@husky.neu.edu>
2017-10-06 00:22:25 -0400

    adding to launch-ec2-instance.sh

--- infrastructure/scripts/launch-ec2-instance.sh
+++ infrastructure/scripts/launch-ec2-instance.sh
@@ -21,3 +21,22 @@ aws ec2 authorize-security-group-ingress --group-name csye6225-fall2017-webapp -
 aws ec2 authorize-security-group-ingress --group-name csye6225-fall2017-webapp --protocol tcp --port 443 --cidr 0.0.0.0/0
 
 echo "inbound rules modified"
+
+#get subnet id
+export SUBNET_ID=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPC_ID" --query "Subnets[0].SubnetId" --output text)
+
+#get INSTANCE_ID
+export INSTANCE_ID=$(aws ec2 run-instances --image-id ami-cd0f5cb6 --count 1 --instance-type t2.micro --key-name csye6225-aws --security-group-ids $SG_ID --subnet-id $SUBNET_ID --block-device-mappings "[{\"DeviceName\":\"/dev/sdf\",\"Ebs\":{\"VolumeSize\":16,\"VolumeType\":\"gp2\"}}]" --disable-api-termination --query "Instances[0].InstanceId" --output text)
+
+echo in
+
+#wait until insance is in running state
+aws ec2 wait --region us-east-1 instance-running --instance-ids $INSTANCE_ID
+
+#fetch IP address
+export IP_ADDRESS=$(aws ec2 describe-instances --filters "Name=instance-id,Values=$INSTANCE_ID" --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+echo $IP_ADDRESS
+
+#create record set
+aws route53 change-resource-record-sets --hosted-zone-id $1 --change-batch "{\"Changes\":[{\"Action\":\"UPSERT\",\"ResourceRecordSet\":{\"Name\":\"$2\",\"Type\":\"A\",\"TTL\":60,\"ResourceRecords\":[{\"Value\":\"$IP_ADDRESS\"}]}}]}"
+
