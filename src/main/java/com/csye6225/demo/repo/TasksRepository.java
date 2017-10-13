11c74805c796a6e82cbdb268429811505d7e2bb1
Nirali <merchant.n@husky.neu.edu>
2017-10-12 23:02:30 -0400

    Adding 403 for /tasks/attachments

--- src/main/java/com/csye6225/demo/repo/TasksRepository.java
+++ src/main/java/com/csye6225/demo/repo/TasksRepository.java
@@ -7,9 +7,6 @@
 
 package com.csye6225.demo.repo;
 
-
-
-
 import com.csye6225.demo.pojo.Tasks;
 import com.csye6225.demo.pojo.User;
 import org.springframework.data.repository.CrudRepository;
@@ -20,10 +17,11 @@ import java.util.List;
 @Repository
 public interface TasksRepository extends CrudRepository<Tasks,Long> {
 
-     Tasks findByTaskId(String id);
+    Tasks findByTaskId(String id);
     Tasks findTasksByTaskId(String taskId);
-
     List<Tasks> findTasksByUser(User user);
+
+    Tasks findTaskByTaskId(String taskId);
 
 
 }
