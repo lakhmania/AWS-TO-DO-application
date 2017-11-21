af97fa046ea0dffaf6bdfc7c3f3fbe97afe295be
chintan koticha <koticha.c@husky.neu.edu>
2017-11-20 21:18:17 -0500

    change cloud formation script awslogs.conf

--- restartawslogsservice.sh
+++ restartawslogsservice.sh
@@ -3,6 +3,6 @@
 sudo cp awslogs.conf /var/awslogs/etc
 sudo service awslogs stop
 sleep 60
+sudo service awslogs restart
+sleep 120
 sudo service awslogs start
-sleep 60
-sudo service awslogs status
