c0d7980abf5c85d8a2bb2a8583b0f258af2c0c7b
Apoorva Lakhmani <lakhmani.a@husky.neu.edu>
2017-12-06 18:49:56 -0500

    changes for firewall

--- infrastructure/gcp/creategcpdeploment.sh
+++ infrastructure/gcp/creategcpdeploment.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 gcloud deployment-manager deployments create my-deployment \
-   --template vm_template.jinja \
-   --properties zone:us-east1-b,machineType:n1-standard-1,containerImage:ubuntu-1604-lts
+--template vm_template.jinja \
+--properties zone:us-east1-b,machineType:n1-standard-1,containerImage:ubuntu-1604-lts,\
+tag:network-tag,roleId:aCustomRole,title:MyTitle,description:Mydescription,stage:ALPHA
