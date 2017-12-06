576f6806fa4e979fe2eea6dad9472b6a68711e93
Nirali <merchant.n@husky.neu.edu>
2017-12-05 22:52:16 -0500

    Load balancer

--- infrastructure/gcp/creategcpdeploment.sh
+++ infrastructure/gcp/creategcpdeploment.sh
@@ -2,4 +2,4 @@
 
 gcloud deployment-manager deployments create my-deployment \
    --template vm_template.jinja \
-   --properties zone:us-east1-b,machineType:n1-standard-1,containerImage:ubuntu-1604-lts,storage:csye6225-storage
+   --properties zone:us-east1-b,machineType:n1-standard-1,containerImage:ubuntu-1604-lts,storage:csye6225-storage,port:8080,service:http
