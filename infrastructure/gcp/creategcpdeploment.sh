a42d0477f3632fa7b2784554b44df95315488a1f
Neha Lalwani <lalwani.n@husky.neu.edu>
2017-12-05 22:02:07 -0500

    autoscaling group

--- infrastructure/gcp/creategcpdeploment.sh
+++ infrastructure/gcp/creategcpdeploment.sh
@@ -2,4 +2,4 @@
 
 gcloud deployment-manager deployments create my-deployment \
    --template vm_template.jinja \
-   --properties zone:us-east1-b,machineType:n1-standard-1,containerImage:ubuntu-1604-lts
+   --properties zone:us-east1-b,machineType:n1-standard-1,containerImage:ubuntu-1604-lts,region:us-east1
