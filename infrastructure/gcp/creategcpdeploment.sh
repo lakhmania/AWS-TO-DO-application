99f91a07b043c91299ebb02509784526e16cd5a0
Apoorva Lakhmani <lakhmani.a@husky.neu.edu>
2017-12-07 22:58:34 -0500

    added ssl certificate

--- infrastructure/gcp/creategcpdeploment.sh
+++ infrastructure/gcp/creategcpdeploment.sh
@@ -3,6 +3,5 @@
 gcloud deployment-manager deployments create my-deployment \
 --template vm_template.jinja \
 --properties zone:us-east1-b,machineType:n1-standard-1,containerImage:ubuntu-1604-lts,\
-tag:network-tag,serviceAccountId:csye6225,roleId:aCustomRole,title:MyTitle,description:Mydescription,stage:ALPHA \
-storage:csye6225-storage,port:8080,service:http,region:us-east1
+tag:network-tag,serviceAccountId:csye6225,roleId:aCustomRole,title:MyTitle,description:Mydescription,stage:ALPHA,storage:csye6225-storage,port:8080,service:http,region:us-east1
 
