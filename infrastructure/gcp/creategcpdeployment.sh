9a1f947b0b6d54a159b334ed2de2498482fd9285
chintan koticha <koticha.c@husky.neu.edu>
2017-12-08 20:36:27 -0500

    bigtable cmd creation

--- infrastructure/gcp/creategcpdeployment.sh
+++ infrastructure/gcp/creategcpdeployment.sh
@@ -1,2 +1,9 @@
 gcloud deployment-manager deployments create my-igm \
     --template vm_bigtable.jinja
+
+gcloud beta bigtable instances create csye6225-instance --cluster=csye6225-cluster --cluster-zone=us-central1-b --instance-type=PRODUCTION --cluster-num-nodes=3 --cluster-storage-type=SSD --description=csye6225-instance
+echo -e "instance = csye6225-instance" > ~/.cbtrc
+cbt createtable csye6225-table 10,20
+cbt createfamily csye6225-table cf1
+
+
