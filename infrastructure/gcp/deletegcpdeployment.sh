0c876cf6ab78b7fd1e80acd8cd4a144c5441e1a7
chintan koticha <koticha.c@husky.neu.edu>
2017-12-11 18:30:51 -0500

    further correction create gcp script

--- infrastructure/gcp/deletegcpdeployment.sh
+++ infrastructure/gcp/deletegcpdeployment.sh
@@ -1,5 +1,4 @@
 gcloud deployment-manager deployments delete $1 --async --delete-policy=delete
 
-
-gcloud beta bigtable instances delete csye6225-instance
 cbt deletetable csye6225-table
+gcloud beta bigtable instances delete csye6225-instance
