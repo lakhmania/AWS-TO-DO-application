0abf9e5f3d5012bf3d684ef8b4e11be3ab3e7ec1
Nirali <merchant.n@husky.neu.edu>
2017-12-15 01:20:47 -0500

    changed certificate name

--- infrastructure/gcp/creategcpdeployment.sh
+++ infrastructure/gcp/creategcpdeployment.sh
@@ -2,7 +2,7 @@
 
 gcloud deployment-manager deployments create my-igm \
     --template vm_template.jinja \
-    --properties zone:us-east1-b,machineType:n1-standard-1,containerImage:ubuntu-1604-lts,storage:csye6225-storage,port:80,service:http,region:us-east1,sslCertificateName:csye6225-certificate,httpsport:443,tag:network-tag,instancename:$1,dnsName:$2
+    --properties zone:us-east1-b,machineType:n1-standard-1,containerImage:ubuntu-1604-lts,storage:csye6225-storage,port:80,service:http,region:us-east1,sslCertificateName:csye6225certificate,httpsport:443,tag:network-tag,instancename:$1,dnsName:$2
 
 gcloud beta bigtable instances create csye6225-instance --cluster=csye6225-cluster --cluster-zone=us-central1-b --instance-type=PRODUCTION --cluster-num-nodes=3 --cluster-storage-type=SSD --description=csye6225-instance
 echo -e "instance = csye6225-instance" > ~/.cbtrc
--- infrastructure/gcp/deletegcpdeployment.sh
+++ infrastructure/gcp/deletegcpdeployment.sh
@@ -1,10 +1,10 @@
 export ip=$(gcloud compute forwarding-rules describe csye6225-globalforwarding-rule --global | grep IPAddress)
 export ip=$(cut -d ":" -f 2 <<< "$ip")
 gcloud dns record-sets transaction start -z=csye6225-managed-zone
-gcloud dns record-sets transaction remove --zone csye6225-managed-zone --name $2 --ttl 60 --type A "$ip"
+gcloud dns record-sets transaction remove --zone csye6225-managed-zone --name $1 --ttl 60 --type A "$ip"
 gcloud dns record-sets transaction execute -z=csye6225-managed-zone
 
-gcloud deployment-manager deployments delete $1 --async --delete-policy=delete
+gcloud deployment-manager deployments delete my-igm --async --delete-policy=delete
 
 cbt deletetable csye6225-table
 gcloud beta bigtable instances delete csye6225-instance
